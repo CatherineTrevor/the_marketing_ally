c5b6f55d035738a1bff6bff4dd7ce7c2508dd338
Catherine Trevor <catherinetrevor@outlook.com>
2021-09-27 11:07:15 +0000

    remove secret key and set debug

--- marketing_ally/settings.py
+++ marketing_ally/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gtzt)a!c0t10!hwsc&f^y$o4)+p#tid%5h0va73cs6l&nn7#td'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-marketing-ally', 'localhost']
 
