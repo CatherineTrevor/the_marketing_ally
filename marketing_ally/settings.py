edbd899685a9047cd3f149a32b55738ac24b8bbb
Catherine Trevor <catherinetrevor@outlook.com>
2021-09-28 07:12:24 +0000

    add order_basket forms and migrate with basic form content

--- marketing_ally/settings.py
+++ marketing_ally/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'contact',
+    'order_basket',
 
     # other
 
@@ -87,7 +88,7 @@ TEMPLATES = [
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
                 'crispy_forms.templatetags.crispy_forms_field',
-            ]            
+            ]
         },
     },
 ]
@@ -173,7 +174,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
@@ -195,7 +195,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'    
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- order_basket/views.py
+++ order_basket/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.conf import settings
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .forms import OrderRequestForm
+from .models import OrderRequest
+
+
+def order_basket(request):
+
+    if request.method == 'POST':
+
+        form_data = {
+            'account_company_name': request.POST['company_name'],
+            'timeslot_option_1': request.POST['timeslot_option_1'],
+            'timeslot_option_2': request.POST['timeslot_option_2'],
+            'project_name': request.POST['project_name'],
+            'project_description': request.POST['project_description'],
+        }
+        order_request_form = OrderRequestForm(form_data)
+
+        if order_request_form.is_valid():
+            form = order_request_form.save(commit=False)
+            form.save()
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('home'))
+
+    order_request_form = OrderRequestForm()
+    template = 'order_basket/order_basket.html'
+    context = {
+        'order_request_form': order_request_form,
+    }
+
+    return render(request, template, context)
