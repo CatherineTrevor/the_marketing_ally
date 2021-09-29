d4518809409ff555501ac7d26c807d3031f28523
Catherine Trevor <catherinetrevor@outlook.com>
2021-09-29 12:34:17 +0000

    add basic stripe payment intent functionality to order_basket

--- order_basket/views.py
+++ order_basket/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect, reverse
 from django.conf import settings
+from django.contrib import messages
 from django.views.decorators.http import require_POST
 
 from .forms import OrderRequestForm
 from .models import OrderRequest
 
+import stripe
+
 
 def order_basket(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
 
@@ -26,12 +31,23 @@ def order_basket(request):
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('home'))
 
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=100,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_request_form = OrderRequestForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it?')
+
     template = 'order_basket/order_basket.html'
     context = {
         'order_request_form': order_request_form,
-        'stripe_public_key': 'pk_test_51JXPgOAz3izc7KdhHtbrfowpOlxVe3gGMyEh84gBe3nyNpAxDd4VW1Qx017TSCk85GWSEDLqaSXQb96PeytE9hrQ00lbNXI96U',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
