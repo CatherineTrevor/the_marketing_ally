8786a79eb7f5c4ec05c08f426ec811f4aabdad46
Catherine Trevor <catherinetrevor@outlook.com>
2021-09-30 07:15:03 +0000

    create basic views, html files without content and migrate product app

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.products, name='products'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<int:product_id>', views.edit_product, name='edit_product'),
+]
