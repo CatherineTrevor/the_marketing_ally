c64345e71019ab9f07b5305a8f924957cacc15f1
Catherine Trevor <catherinetrevor@outlook.com>
2021-10-05 07:01:00 +0000

    update pricing functionality to connect to product app, using product priorities

--- products/models.py
+++ products/models.py
@@ -25,15 +25,28 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    priority = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+    )
+
     category = models.ForeignKey('Category', null=True,
                                  on_delete=models.SET_NULL, default="")
     name = models.CharField(max_length=254)
+    description_1 = models.CharField(max_length=100, blank=True)
+    description_2 = models.CharField(max_length=100, blank=True)
+    description_3 = models.CharField(max_length=100, blank=True)
+    description_4 = models.CharField(max_length=100, blank=True)    
     price = models.DecimalField(max_digits=6, decimal_places=0)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     time_allocation_mins = models.DecimalField(max_digits=6, decimal_places=0, blank=True, null=True)
     is_digital = models.BooleanField(null=False)
     max_time_multiplier = models.DecimalField(max_digits=6, decimal_places=1, blank=True, null=True)
+    pricing_priority = models.DecimalField(max_digits=6, decimal_places=0, null=True, default='4', choices=priority)
 
     objects = models.Manager()
 
