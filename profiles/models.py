3bc7762d1e2b93c9327a0647b39f9f95c85ea246
Catherine Trevor <catherinetrevor@outlook.com>
2021-10-07 17:22:02 +0000

    update settings.py for Heroku deployment

--- profiles/models.py
+++ profiles/models.py
@@ -31,6 +31,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
@@ -38,5 +39,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-   # Existing users: just save the profile
+    # Existing users: just save the profile
     instance.userprofile.save()
